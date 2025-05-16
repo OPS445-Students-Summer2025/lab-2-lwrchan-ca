119c2201bbb34a64baf975ecfd499f35af7e737d
Leung Wai Rene Chan <lwrchan@myseneca.ca>
2025-05-16 17:10:14 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author: Leung Wai Rene Chan
+#Author ID: 160682231
+#Date Created: 2025/05/16
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer -1
+print('blast off!')
+
+
+
