264f887f3d125d69980f684037c371db79eb65ea
Leung Wai Rene Chan <lwrchan@myseneca.ca>
2025-05-16 17:10:30 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Author: Leung Wai Rene Chan
+#Author ID: 160682231
+#Date Created: 2025/05/16
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer -1
+print('blast off!')
+
+
+
